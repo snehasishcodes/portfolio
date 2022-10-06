aee48b7eaa40ca464697b78e7981e01fadedf53a
Snehasish Codes <113252136+snehasishcodes@users.noreply.github.com>
2022-10-06 23:09:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,1 @@
-document.addEventListener("DOMContentLoaded", function () {new TypeIt("#type", {strings: ["A teen from India who sucks at everything TwT"],}).go();});
+document.addEventListener("DOMContentLoaded", function () {new TypeIt("#type", {strings: ["Full Stack Web Developer"],}).go();});
